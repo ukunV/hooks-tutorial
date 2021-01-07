d898a02fbd835ae8309f28ed511beb2b2ef0ceb0
ukunV <showklee@naver.com>
2021-01-07 23:20:02 +0900

    21/01/07_add
    21/01/07_add

---
+++ src/Info_useInputs.js
@@ -0,0 +1,34 @@
+import React from "react";
+import useInputs from "./useInputs";
+
+const Info = () => {
+  const [state, dispatch] = useInputs({
+    name: "",
+    nickname: "",
+  });
+
+  const { name, nickname } = state;
+
+  const onChange = (e) => {
+    dispatch(e.target);
+  };
+
+  return (
+    <div>
+      <div>
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
+      </div>
+      <div>
+        <div>
+          <b>이름:</b> {name}
+        </div>
+        <div>
+          <b>닉네임: </b> {nickname}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
